664db32c6e9d705b2c1b9f27235a39f004eb2dca
Henry Lay <henry88lay@gmail.com>
2019-01-19 15:41:52 +1100

    Project workings

--- js/script.js
+++ js/script.js
@@ -6,42 +6,164 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+//Global Variables
+document.addEventListener('DOMContentLoaded', () => {
+  //Declare Global Variables
+  const pageContainer = document.querySelector('.page');
+  const pageHeader = document.querySelector('.page-header');
+  const classList = document.querySelector('.student-list')
+  const students = classList.children;
+
+  //Show and hide page items relating to page selected
+  const showPage = (list, page) => {
+    const upper = page * 10;
+    const lower = upper - 10;
+     for (let i=0; i < list.length; i++) {
+    if (i >= lower && i < upper) {
+      list[i].style.display = 'block';
+    } else {
+      list[i].style.display = 'none';
+    } 
+  }
+}
 
 
+  //Hide all students
+  const hideAllStudents = (list) => {
+    for (let i = 0; i < list.length; i++) {
+      const li = list[i];
+      li.style.display = 'none';
+    }
+  }
 
+  //Create new elements helper function
+  const createElement = (element, attr, value) => {
+    const elem = document.createElement(element);
+    elem[attr] = value;
+    return elem;
+  }
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+  //Append new elements helper function
+  const appendNewElem = (parent, elementName, property, value) => {
+    const elem = createElement(elementName, property, value);
+    parent.appendChild(elem);
+    return elem;
+  }
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+  //Create and add search form to the page
+  const createSearchForm = () => {
+    const div = createElement('div', 'classList', 'student-search');
+    const input = createElement('input', 'type', 'text');
+    input.placeholder = 'Search for students...';
+    div.appendChild(input);
+    appendNewElem(div, 'button', 'textContent', 'Search');
+    pageHeader.appendChild(div);
+  }
 
+  //Construct html of pagination link
+  const createLink = text => {
+    const li = document.createElement('li');
+    const a = createElement('a', 'textContent', text);
+    a.setAttribute('href', '#');
+    li.appendChild(a);
+    return li;
+  }
 
+  //Remove pagination links
+  const removePagination = () => {
+    const pagination = document.querySelector('.pagination');
+    if (pagination) {
+      pageContainer.removeChild(pagination);
+    }
+  }
 
+  //Remove no results message
+  const removeErrorMsg = () => {
+    const errorDiv = document.querySelector('.error-message');
+    if (errorDiv) {
+      errorDiv.parentNode.removeChild(errorDiv);
+    }
+  }
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+  //Create pagination links
+  const createPagination = list => {
+    const pages = Math.ceil(list.length / 10);
+    removePagination();
+    if (pages > 1) {
+      const div = createElement('div', 'classList', 'pagination');
+      const ul = document.createElement('ul');
+      for (let i = 1; i <= pages; i++) {
+        const link = createLink(i);
+        ul.appendChild(link);
+      }
+      ul.firstChild.children[0].classList.add('active');
+      div.appendChild(ul);
+      pageContainer.appendChild(div);
+      return div;
+    }
+  }
+
+  //Add event listener to pagination links
+  const appendPageLinks = (list) => {
+    const div = createPagination(list);
+    div.addEventListener('click', (e) => {
+      e.preventDefault();
+      const activeLink = e.target;
+      if (activeLink.tagName === 'A') {
+        const pageNumber = parseInt(activeLink.textContent);
+        const links = document.querySelectorAll('.pagination a');
+        showPage(list, pageNumber);
+        for (let i = 0; i < links.length; i++) {
+          if (links[i] == activeLink) {
+            links[i].classList.add('active');
+          } else {
+            links[i].classList.remove('active');
+          }
+        }
+      }
+    });
+  }
+
+  const filtering = () => {
+    const inputValue = document.querySelector('input').value;
+    removeErrorMsg();
+    const results = [];
+    for (let i = 0; i < students.length; i++) {
+      const li = students[i];
+      const h3 = li.querySelector('h3');
+      if (h3.textContent.includes(inputValue)) {
+        results.push(li);
+      }
+    }
+    hideAllStudents(students);
+    if (results.length == 0) {
+      removePagination();
+      const errorDiv = createElement('div', 'classList', 'error-message');
+      errorDiv.textContent = "There are not matching results to your search.";
+      pageContainer.appendChild(errorDiv);
+    } else if(results.length < 10) {
+      removePagination();
+      showPage(results, 1);
+    }  else {
+      showPage(results, 1);
+      appendPageLinks(results);
+    }
+  }
+
+  //Initialize pagination on page load
+  createSearchForm();
+  showPage(students, 1);
+  appendPageLinks(students);
+
+  //Add event listener for search input
+  document.querySelector('input').addEventListener('keyup', function () {
+    filtering();
+  });
+  document.querySelector('.student-search button').addEventListener('click', function () {
+    filtering();
+  });
+});
+
 
 
 
